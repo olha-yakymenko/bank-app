0d05ea47dfd96267fd551b8a5f17fe2191ff7736
Olha Yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-10-09 17:06:22 +0200

    3

--- app/Konto.py
+++ app/Konto.py
@@ -1,7 +1,10 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel):
         self.imie=imie
         self.nazwisko=nazwisko
         self.saldo=0
-        self.pesel="12345678910"
+        if len(pesel)==11:
+            self.pesel=pesel
+        else:
+            self.pesel="Niepoprawny pesel"
         pass

57d9a38d687e5c2cc1511d61df4e9ad3cbc0f9fa
Olha Yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-10-09 16:53:27 +0200

    2

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,6 @@
 class Konto:
-    def __init__(self):
+    def __init__(self, imie, nazwisko):
+        self.imie=imie
+        self.nazwisko=nazwisko
+        self.saldo=0
         pass
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,11 +3,14 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
-
+    imie="Dariusz"
+    nazwisko="Januszewski"
+    saldo=0
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        pierwsze_konto = Konto(self.imie, self.nazwisko)
+        self.assertEqual(pierwsze_konto.imie, self.imie, "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, self.nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+
 
     #tutaj proszę dodawać nowe testy

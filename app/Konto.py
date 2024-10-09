5b19a13ee7529973bdca3122f366ded8c4586257
Olha Yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-10-09 17:33:13 +0200

    4

--- app/Konto.py
+++ app/Konto.py
@@ -1,10 +1,22 @@
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, kod=None):
         self.imie=imie
         self.nazwisko=nazwisko
-        self.saldo=0
+
         if len(pesel)==11:
             self.pesel=pesel
         else:
             self.pesel="Niepoprawny pesel"
-        pass
+        if self.czy_kod_poprawny(kod):
+            self.saldo=50
+        else :
+            self.saldo=0
+
+
+    def czy_kod_poprawny(self, kod):
+        if kod is None:
+            return False
+        if kod.startswith("PROM_") and len(kod)==8:
+            return True
+        else :
+            return False
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -21,3 +21,15 @@ class TestCreateBankAccount(unittest.TestCase):
         dlugi_pesel="123878669696966969"
         konto=Konto(self.imie, self.nazwisko, dlugi_pesel)
         self.assertEqual(konto.pesel, "Niepoprawny pesel", "Pesel nie zostal zapisany")
+
+    def test_zly_kod_sufix(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "Prombkjjgkghgg")
+        self.assertEqual(konto.saldo,0, "Kod promocyjny jest zly")
+
+    def test_dobry_kod_sufix(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_123")
+        self.assertEqual(konto.saldo, 50, "Kod promocyjny jest dobry")
+
+    def test_poczatek_kod_sufix(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, "PROM_876586")
+        self.assertEqual(konto.saldo, 0, "Kod promocyjny jest zly")

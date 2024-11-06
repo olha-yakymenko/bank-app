cce46b7a82707a3d141f1275f9979406053bae62
Olha Yakymenko <o.yakymenko.450@studms.ug.edu.pl>
2024-11-06 17:12:20 +0100

    coverage

--- app/PersonalAccount.py
+++ app/PersonalAccount.py
@@ -4,7 +4,6 @@ class PersonalAccount(Konto):
     def __init__(self, imie, nazwisko, pesel, kod=None):
         self.imie=imie
         self.nazwisko=nazwisko
-        self.saldo=0
 
         if len(pesel)==11:
             self.pesel=pesel
@@ -24,18 +23,13 @@ class PersonalAccount(Konto):
             return False
 
     def czy_po_roku_1960(self, pesel):
-        year = int(pesel[0:2])
-        month = int(pesel[2:4])
+        rok_urodzenia = int(pesel[0:2])
+        miesiac = int(pesel[2:4])
 
-        if 1 <= month <= 12:
-            year += 1900
-        elif 21 <= month <= 32:
-            year += 2000
-        elif 81 <= month <= 92:
-            year += 1800
-
-        return year > 1960
-
+        if(rok_urodzenia >=61 and (miesiac >=1 and miesiac <=32 )) or (rok_urodzenia <=24 and (miesiac >=1 and miesiac <=32)):
+            return True
+        else:
+            return False
 
 
 
